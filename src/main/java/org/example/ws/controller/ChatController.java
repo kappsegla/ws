45243bbc4b3fba3e6be262f79241987f9d8b350d
Martin Blomberg <martin.blomberg@outlook.com>
2023-12-26 21:29:17 +0100

    Initial Commit

---
+++ src/main/java/org/example/ws/controller/ChatController.java
@@ -0,0 +1,38 @@
+package org.example.ws.controller;
+
+import org.example.ws.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ChatController {
+    @GetMapping("/")
+    public String chatPage() {
+        return "index.html";
+    }
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.unregister")
+    @SendTo("/topic/public")
+    public ChatMessage unregister(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().remove("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
